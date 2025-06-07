29e4556271954d39fac9ccc9293bc6a914125381
Killar.exe <killar.exe@gmail.com>
2025-06-07 11:09:13 +0200

    Crystals 1.3.0
    Add Redstone, Quartz & Glowstone geodes

--- fabric/src/main/java/github/killarexe/crystals/fabric/registry/CrystalsModFabricFeatures.java
+++ fabric/src/main/java/github/killarexe/crystals/fabric/registry/CrystalsModFabricFeatures.java
@@ -21,6 +21,10 @@ public class CrystalsModFabricFeatures {
 	public static final ResourceKey<PlacedFeature> GLOWSTONE_CRYSTALS_FEATURE = createKey("glowstone_crystals");
 	public static final ResourceKey<PlacedFeature> QUARTZ_CRYSTALS_FEATURE = createKey("quartz_crystals");
 
+	public static final ResourceKey<PlacedFeature> REDSTONE_GEODE = createKey("redstone_geode");
+	public static final ResourceKey<PlacedFeature> QUARTZ_GEODE = createKey("quartz_geode");
+	public static final ResourceKey<PlacedFeature> GLOWSTONE_GEODE = createKey("glowstone_geode");
+
 	private static ResourceKey<PlacedFeature> createKey(String id) {
 		return ResourceKey.create(Registries.PLACED_FEATURE, CrystalsMod.id(id));
 	}
@@ -34,5 +38,9 @@ public class CrystalsModFabricFeatures {
 		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Decoration.UNDERGROUND_DECORATION, REDSTONE_CRYSTALS_FEATURE);
 		BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Decoration.UNDERGROUND_DECORATION, GLOWSTONE_CRYSTALS_FEATURE);
 		BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Decoration.UNDERGROUND_DECORATION, QUARTZ_CRYSTALS_FEATURE);
+
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Decoration.UNDERGROUND_DECORATION, REDSTONE_GEODE);
+		BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Decoration.UNDERGROUND_DECORATION, QUARTZ_GEODE);
+		BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Decoration.UNDERGROUND_DECORATION, GLOWSTONE_GEODE);
 	}
 }
